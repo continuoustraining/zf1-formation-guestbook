b7ca0b60492cb6877932881fe706d5269e57e985
ppaulis <ppaulis@gmail.com>
2017-09-05 12:14:29 +0200

    moved autoloading completely to composer

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,20 +9,6 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     /**
-     * Bootstrap autoloader for application resources
-     * 
-     * @return Zend_Application_Module_Autoloader
-     */
-    protected function _initAutoload()
-    {
-        $autoloader = new Zend_Application_Module_Autoloader(array(
-            'namespace' => 'Default',
-            'basePath'  => dirname(__FILE__),
-        ));
-        return $autoloader;
-    }
-
-    /**
      * Bootstrap the view doctype
      * 
      * @return void
